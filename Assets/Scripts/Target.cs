279831c5077e19799917164ebec5965b76d543f9
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-12-01 21:50:00 +0530

    Target and weaponManager

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public Animator animator;
+
+    public float health = 50f;
+    public bool die = false;
+
+   
+    
+    public void TakeDamage(float amount)
+    {
+        
+        health -= amount;
+        if(amount >= 25f && health <= 0f)
+        {
+            die = true;
+            animator.SetBool("diehead", true);
+        }
+        if (health <= 0f && amount < 25f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        die = true;
+        animator.SetBool("die", true);
+        // Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/weaponManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponManager : MonoBehaviour
+{
+
+    public int selectedweapon = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectWeapon();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int previousselectedweapon = selectedweapon;
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (selectedweapon >= transform.childCount - 1)
+                selectedweapon = 0;
+            else
+            selectedweapon++;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (selectedweapon <= 0)
+
+                selectedweapon = transform.childCount - 1;
+            else
+                selectedweapon--;
+        }
+        bool run = Input.GetButton("run");
+        if (previousselectedweapon != selectedweapon && !run)
+            selectWeapon();
+    }
+
+    void selectWeapon()
+    {
+        int i = 0;
+
+        foreach(Transform weapon in transform)
+        {
+            if (i == selectedweapon)
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
+
+            i++;
+        }
+    }
+}
