4a51e5f9c38ecdb131a683933e783807f4be838a
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-12-07 23:44:32 +0530

    enemy scripts

---
+++ Assets/Scripts/enemy/EnemyController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float lookRadius = 10f;
+    Transform target;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = PlayerManger.instance.player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                FaceTarget();
+                //attack
+            }
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookrotation = Quaternion.LookRotation( new Vector3( direction.x, 0, direction.z) );
+        transform.rotation =  Quaternion.Slerp(transform.rotation , lookrotation,Time.deltaTime*5f);
+    }
+}
