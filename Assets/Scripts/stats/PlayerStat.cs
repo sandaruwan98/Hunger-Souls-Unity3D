187baa326dd46952fd84de49fb14c1f151ce1619
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-12-11 11:54:59 +0530

    CharactorStat and playerstats

---
+++ Assets/Scripts/stats/PlayerStat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : CharactorStat
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+        // show blood ui to show you have hit
+    }
+
+    public override void Die()
+    {
+        base.Die();
+        //animation 
+        // display lost game
+        // restrat or goto menu
+    }
+}
