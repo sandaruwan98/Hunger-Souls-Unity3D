e4aa01b8d7bd27fd3d983e4c3c1f247b77311183
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-12-15 20:05:47 +0530

    mainmenu.cs

---
+++ Assets/Scripts/menu/MainMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject MainPanel;
+    public GameObject OptionsPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    //for main panel
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Options()
+    {
+        OptionsPanel.SetActive(true);
+        MainPanel.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+
+    //for options
+    public void IsFullScreen(bool isFull)
+    {
+        
+    }
+
+    public void Back()
+    {
+        OptionsPanel.SetActive(false);
+        MainPanel.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
