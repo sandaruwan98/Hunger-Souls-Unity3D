42cc744714f78369516126428c3e15487bb79161
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-12-17 12:32:11 +0530

    BloodPanel script

---
+++ Assets/Scripts/menu/BloodPanel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BloodPanel : MonoBehaviour
+{
+    CanvasGroup canva;
+    bool isShowing = false;
+    public float duration = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canva = GetComponent<CanvasGroup>();
+        
+    }
+
+    public void DisplayPanel()
+    {
+        if(!isShowing)
+            StartCoroutine(Fade());
+        //isShowing = !isShowing;
+    }
+
+
+    public IEnumerator Fade()
+    {
+        isShowing = true;
+        float start = canva.alpha;
+        float counter = 0f;
+        float finduration = duration * 0.25f;
+        float foutduration = duration * 0.75f;
+        while (counter < finduration)
+        {
+            counter += Time.deltaTime;
+            canva.alpha = Mathf.Lerp(0f,1f,counter/ finduration);
+            yield return null;
+        }
+        counter = 0f;
+        start = canva.alpha;
+        while (counter < foutduration)
+        {
+            counter += Time.deltaTime;
+            canva.alpha = Mathf.Lerp(1f, 0f, counter / foutduration);
+            yield return null;
+        }
+        isShowing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
