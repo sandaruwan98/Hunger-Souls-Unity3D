242c4af53d02fef0c4c2d03577652b497bdfafb5
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-12-18 09:40:07 +0530

    script

---
+++ Assets/Scripts/menu/WinMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinMenu : MonoBehaviour
+{
+
+    public void Mainmenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
