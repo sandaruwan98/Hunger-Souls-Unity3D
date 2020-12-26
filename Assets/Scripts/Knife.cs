bf0acc5a84a5cb2458b39a6f5dddc34816b5f128
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-12-26 17:58:15 +0530

    knife attack completed

--- Assets/Scripts/Knife.cs
+++ Assets/Scripts/Knife.cs
@@ -8,11 +8,18 @@ public class Knife : MonoBehaviour
     private bool walk;
     private bool attack1;
     private bool attack2;
+    private bool isAttacking = false;
+    public float attackCoolDown = -1f;
+    public float attackTime = 1f;
     public Animator animator;
+    public BloodEffect BloodEffect;
+    public AudioSource audioSource;
+    public AudioClip attckclip1;
+    public AudioClip attckclip2;
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
         
     }
 
@@ -20,6 +27,8 @@ public class Knife : MonoBehaviour
     {
         //animator.SetTrigger("Idle");
     }
+
+
     // Update is called once per frame
     void Update()
     {
@@ -30,14 +39,32 @@ public class Knife : MonoBehaviour
 
 
 
-        if (attack1)
+        
+
+        if (isAttacking)
         {
-            animator.SetTrigger("Attack_1");
+            attackCoolDown -= Time.deltaTime;
         }
 
-        if (attack2)
+        isAttacking = attackCoolDown > 0 ;
+
+
+        if (attack1 && !isAttacking)
         {
+            isAttacking = true;
+            attackCoolDown = attackTime;
+            animator.SetTrigger("Attack_1");
+            audioSource.clip = attckclip1;
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+
+        if (attack2 && !isAttacking)
+        {
+            isAttacking = true;
+            attackCoolDown = attackTime;
             animator.SetTrigger("Attack_2");
+            audioSource.clip = attckclip2;
+            audioSource.PlayOneShot(audioSource.clip);
         }
 
 
@@ -50,8 +77,28 @@ public class Knife : MonoBehaviour
         }
         else
         {
-            animator.ResetTrigger("Walk");
+            //animator.ResetTrigger("Walk");
             animator.SetTrigger("Idle");
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "ENEMY")
+        {
+            
+            if(isAttacking)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.forward, out hit))
+                {
+                    //Debug.Log("Point of contact: " + hit.point);
+                    BloodEffect.BloodImpact(hit);
+                }
+                other.gameObject.GetComponent<EnemyStat>().Die();
+                
+            }
         }
     }
 }
